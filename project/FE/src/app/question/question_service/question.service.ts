30549449e33464345705b1bbac30c27c46ea5f41
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-09-06 13:59:11 +0700

    quan update question

--- project/FE/src/app/question/question_service/question.service.ts
+++ project/FE/src/app/question/question_service/question.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Question} from '../Question';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionService {
-
-  constructor() { }
+  URL = 'http://localhost:8080';
+  constructor(private httpClient: HttpClient) { }
+  saveQuestion(question: Question): Observable<any>{
+    return this.httpClient.post<any>(this.URL + '/create-question', question);
+  }
+  findAllQuestion(): Observable<Question[]>{
+    return this.httpClient.get<Question[]>(this.URL + '/question');
+  }
+  findById(id: string): Observable<Question>{
+    return this.httpClient.get<Question>(this.URL + '/question/' + id);
+  }
 }
