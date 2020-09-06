5b712c1d5ff715b6a911cd7724b06fb86ab3db36
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-09-06 13:58:01 +0700

    quan update question

--- project/FE/src/app/question/question.module.ts
+++ project/FE/src/app/question/question.module.ts
@@ -1,12 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {QuestionService} from './question_service/question.service';
+import { AddQuestionComponent } from './add-question/add-question.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AddQuestionComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    AddQuestionComponent
   ],
   providers: [QuestionService]
 })
