ed311d1cf47f571e2538e3e1efcbf1c20a11384d
dautphuong <57697417+dautphuong@users.noreply.github.com>
2020-09-07 09:14:32 +0700

    phuong - edit service exam

--- project/FE/src/app/exam/exam_service/exam.service.ts
+++ project/FE/src/app/exam/exam_service/exam.service.ts
@@ -20,7 +20,7 @@ export class ExamService {
   }
 
   findById(examId: number): Observable<Exam> {
-    return this.httpClient.get<Exam>(this.URL + '/get-test/' + examId);
+    return this.httpClient.get<Exam>(this.URL + '/get-exam/' + examId);
   }
 
   // save(exam: Exam): Observable<Exam> {
