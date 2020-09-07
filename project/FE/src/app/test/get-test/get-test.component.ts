ab3e3e328d8c3035194180123ed5b2e202644db8
dautphuong <57697417+dautphuong@users.noreply.github.com>
2020-09-07 09:15:44 +0700

    phuong - update component get test

--- project/FE/src/app/test/get-test/get-test.component.ts
+++ project/FE/src/app/test/get-test/get-test.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Test} from '../test';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {TestService} from '../test_service/test.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-get-test',
@@ -6,10 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./get-test.component.css']
 })
 export class GetTestComponent implements OnInit {
+  test: Test;
+  testForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private testService: TestService, private router: Router) {
+  }
 
   ngOnInit(): void {
+
   }
 
 }
