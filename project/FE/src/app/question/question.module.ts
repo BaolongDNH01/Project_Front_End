e195e2bf2ed65807146e38eec57054c0af92c05d
chauchau123 <45305889+chauchau123@users.noreply.github.com>
2020-09-08 12:20:13 +0700

    Chau update question-list-in-exam

--- project/FE/src/app/question/question.module.ts
+++ project/FE/src/app/question/question.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {AddQuestionComponent} from './add-question/add-question.component';
+// import {AddQuestionComponent} from './add-question/add-question.component';
 
 import {QuestionBankListComponent} from './question-bank-list/question-bank-list.component';
 import {QuestionBankDeleteComponent} from './question-bank-delete/question-bank-delete.component';
@@ -9,12 +9,14 @@ import {QuestionService} from './question.service';
 import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AutocompleteLibModule} from "angular-ng-autocomplete";
+import {QuestionListInTheExamComponent} from './question-list-in-the-exam/question-list-in-the-exam.component';
+
+
 
 
 @NgModule({
 
-  declarations: [QuestionBankListComponent, QuestionBankDeleteComponent, AddQuestionComponent],
+  declarations: [QuestionBankListComponent, QuestionBankDeleteComponent,QuestionListInTheExamComponent],
   imports: [
     CommonModule,
     QuestionRoutingModule,
@@ -22,11 +24,11 @@ import {AutocompleteLibModule} from "angular-ng-autocomplete";
     ReactiveFormsModule,
     BrowserAnimationsModule,
     FormsModule,
-    AutocompleteLibModule
   ],
   exports: [
     QuestionBankListComponent,
-    AddQuestionComponent
+    QuestionListInTheExamComponent
+    // AddQuestionComponent,
   ],
   providers: [
     QuestionService
--- project/FE/src/app/question/question.service.ts
+++ project/FE/src/app/question/question.service.ts
@@ -1,14 +1,17 @@
-import {Injectable} from "@angular/core";
+import {Injectable} from '@angular/core';
 import {Question} from './question';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Test} from "../test/test";
+import {Test} from '../test/test';
+import {QuestionInExam} from './question-in-exam';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionService {
   question: Question;
+  questionInExam: QuestionInExam;
   API_URL = 'http://localhost:8080';
 
   constructor(
@@ -20,6 +23,11 @@ export class QuestionService {
     return this.httpClient.get<Question[]>(this.API_URL + '/question');
   }
 
+  getAllQuestionInExam(): Observable<Question[]> {
+    return this.httpClient.get<Question[]>(this.API_URL + '/question-in-exam');
+  }
+
+
   findById(id: string): Observable<Question> {
     return this.httpClient.get<Question>(this.API_URL + '/question/' + id);
   }
@@ -27,11 +35,24 @@ export class QuestionService {
   deleteQuestion(id: string): Observable<Question> {
     return this.httpClient.delete<Question>(this.API_URL + '/delete-question/' + id);
   }
-  saveQuestion(question: Question): Observable<any>{
+
+  deleteQuestionInExam(id: string): Observable<QuestionInExam> {
+    return this.httpClient.delete<QuestionInExam>(this.API_URL + '/delete-question-in-exam/' + id);
+  }
+
+  saveQuestion(question: Question): Observable<any> {
     return this.httpClient.post<any>(this.API_URL + '/create-question', question);
   }
 
-  getAllTest(): Observable<Test[]>{
+  saveQuestionInExam(questionInExam: QuestionInExam): Observable<any> {
+    return this.httpClient.post<any>(this.API_URL + '/addQuestionInExam', questionInExam);
+  }
+
+  updateQuestion(question: Question): Observable<Question> {
+    return this.httpClient.patch<Question>(this.API_URL + '/edit-question' + question.questionId, question);
+  }
+
+  getAllTest(): Observable<Test[]> {
     return this.httpClient.get<Test[]>(this.API_URL + '/getAllTest');
   }
 }
