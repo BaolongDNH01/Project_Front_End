3db976b68744d431a0652ec932e644cedb788ab6
chauchau123 <45305889+chauchau123@users.noreply.github.com>
2020-09-09 10:00:00 +0700

    DELETE IDEA

--- project/FE/src/app/question/question.ts
+++ project/FE/src/app/question/question.ts
@@ -10,8 +10,18 @@ export class Question {
   testId: [];
   subjectId;
 
-  constructor() {
+  constructor(questionId: string, question: string, answerA: string, answerB: string, answerC: string, answerD: string, rightAnswer: string, testId: [], subjectId) {
+    this.questionId = questionId;
+    this.question = question;
+    this.answerA = answerA;
+    this.answerB = answerB;
+    this.answerC = answerC;
+    this.answerD = answerD;
+    this.rightAnswer = rightAnswer;
+    this.testId = testId;
+    this.subjectId = subjectId;
   }
 
 }
+
 
