6968e5f2e8f11258fda47bc97eefb5089ce19d18
BaolongDNH01 <61400179+BaolongDNH01@users.noreply.github.com>
2020-09-14 20:32:08 +0700

    update get point and time

--- project/FE/src/app/user/list-user/list-user.component.ts
+++ project/FE/src/app/user/list-user/list-user.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {UserService} from '../user_service/user.service';
 import {User} from '../user_model/User';
+import {Exam} from '../../exam/exam';
 
 @Component({
   selector: 'app-list-user',
@@ -13,6 +14,15 @@ export class ListUserComponent implements OnInit {
 
   constructor(private userService: UserService) {
     userService.findAllUser().subscribe(next => this.userList = next);
+  }
+
+  getTotalTime(user: User){
+    let exam: Exam;
+    let totalTime = 0;
+    for (exam of user.examList){
+      totalTime += exam.times;
+    }
+    return totalTime;
   }
 
   ngOnInit(): void {
