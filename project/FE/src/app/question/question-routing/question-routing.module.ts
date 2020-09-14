7375be0cb5c088ab6d81701c19602fbba1d7a988
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-09-14 18:12:44 +0700

    quan

--- project/FE/src/app/question/question-routing/question-routing.module.ts
+++ project/FE/src/app/question/question-routing/question-routing.module.ts
@@ -3,18 +3,14 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {QuestionBankListComponent} from '../question-bank-list/question-bank-list.component';
 import {QuestionBankDeleteComponent} from '../question-bank-delete/question-bank-delete.component';
-import {AddQuestionComponent} from '../add-question/add-question.component';
-import {UpdateQuestionComponent} from "../update-question/update-question.component";
-import {ErrorPageComponent} from '../../error-page/error-page.component';
+import {UpdateQuestionComponent} from '../update-question/update-question.component';
 
 const routes: Routes = [
   {
     path: 'question',
     children: [
       {path: '', component: QuestionBankListComponent},
-      {path: 'question-delete/:id', component: QuestionBankDeleteComponent},
-      {path: 'add-question', component: AddQuestionComponent},
-      {path: 'update-question/:id', component: UpdateQuestionComponent},
+      {path: 'question-delete/:id', component: QuestionBankDeleteComponent}
     ]
   }
 ];
